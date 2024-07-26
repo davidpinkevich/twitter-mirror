2f437f1a54ffa700bbc46e24b955c1cb41e9e00a
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-26 15:49:44 +0300

    feat: add styles home page

---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledHome = styled.div`
+  display: flex;
+`;
+
+const StyledHomeImage = styled.img`
+  max-height: 100vh;
+`;
+
+export { StyledHome, StyledHomeImage };
