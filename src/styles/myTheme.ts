cd0ca41f6d201aa7946aa412eb32e98f7e959b12
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-26 14:59:49 +0300

    feat: add theme

---
+++ src/styles/myTheme.ts
@@ -0,0 +1,28 @@
+import { type DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+  fontSizes: {
+    enter: {
+      h1: '84px',
+      h2: '42px',
+      btn: '20px',
+      p: '16px',
+    },
+    signup: {
+      input: '18px',
+      title: '30px',
+      h3: '18px',
+      btn: '18px',
+    },
+  },
+  colors: {
+    mainBlack: '#000000',
+    mainWhite: '#FFFFFF',
+    ligthGray: '#E4EAED',
+    darkGray: '#666666',
+    mainBlue: '#1DA1F2',
+    mainRed: '#cf0404',
+  },
+};
+
+export { myTheme };
---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface TypesTheme {
+  fontSizes: {
+    enter: {
+      h1: string;
+      h2: string;
+      btn: string;
+      p: string;
+    };
+    signup: {
+      input: string;
+      title: string;
+      h3: string;
+      btn: string;
+    };
+  };
+  colors: {
+    mainBlack: string;
+    mainWhite: string;
+    darkGray: string;
+    ligthGray: string;
+    mainBlue: string;
+    mainRed: string;
+  };
+}
+
+export interface TypesFormSignUp {
+  name: string;
+  phone: string;
+  email: string;
+}
