a77b1018eea13e0cc862e9da7bae25a2bc100217
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-28 22:34:26 +0300

    feat: add constants for date

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
+import { DATE_PERIODS, NAMES_PERIODS } from './datePeriods';
 import { ERRORS } from './errorInfoForm';
+import { PATTERNS } from './formPatterns';
 import { Paths } from './paths';
 
-export { Paths, ERRORS };
+export { Paths, ERRORS, PATTERNS, DATE_PERIODS, NAMES_PERIODS };
