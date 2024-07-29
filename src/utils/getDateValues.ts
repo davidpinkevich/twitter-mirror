d13f81d309b4f82119ed56e2daf92512380283c4
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-29 14:47:58 +0300

    feat: update view date

--- src/utils/getDateValues.ts
+++ src/utils/getDateValues.ts
@@ -1,3 +1,5 @@
+import { DATE_PERIODS } from 'constants/datePeriods';
+
 const getDaysInMonth = (monthId = '', year = '') => {
   const currentYear = new Date().getFullYear();
   const targetYear = year ? parseInt(year) : currentYear;
@@ -20,15 +22,24 @@ const getDaysInMonth = (monthId = '', year = '') => {
   }));
 };
 
-const getYears = (value: number) => {
+const getYears = () => {
   const currentYear = new Date().getFullYear();
   const years = [];
 
-  for (let i = value; i <= currentYear; i++) {
-    years.push({ id: String(i - value), name: i.toString() });
+  for (let i = DATE_PERIODS.startYear; i <= currentYear; i++) {
+    years.push({ id: String(i - DATE_PERIODS.startYear), name: i.toString() });
   }
 
   return years.reverse();
 };
 
-export { getDaysInMonth, getYears };
+const stringDate = (date: { day: string; month: string; year: string }) => {
+  const day = Number(date.day) + 1 < 10 ? `0${Number(date.day) + 1}` : `${Number(date.day) + 1}`;
+  const month =
+    Number(date.month) + 1 < 10 ? `0${Number(date.month) + 1}` : `${Number(date.month) + 1}`;
+  const year = getYears().find((item) => item.id === date.year)?.name;
+
+  return `${day}.${month}.${year}`;
+};
+
+export { getDaysInMonth, getYears, stringDate };
