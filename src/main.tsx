c3b9bbcc3b83b2e287ed7a038be986c7afcde939
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-29 12:47:01 +0300

    feat: add store

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { persistor, store } from 'data/store';
 import { GlobalStyles } from 'styles/GlobalStyles';
 import { myTheme } from 'styles/myTheme';
 
@@ -9,10 +12,14 @@ import { App } from 'components/App/App';
 import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <ThemeProvider theme={myTheme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <ThemeProvider theme={myTheme}>
+          <GlobalStyles />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
