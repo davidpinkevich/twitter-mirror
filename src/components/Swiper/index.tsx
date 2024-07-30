a065447f373e87db12337a0fdbfa61f4771feb6c
davidpinkevich <davidpinkevich95@gmail.com>
2024-07-30 13:04:24 +0300

    feat: add swiper theme

---
+++ src/components/Swiper/index.tsx
@@ -0,0 +1,21 @@
+import { changeTheme, getTheme } from 'data/slices/sliceMemory';
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
+
+import { StyledSwiper, StyledSwiperBtn } from './styled';
+
+const Swiper: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const theme = useAppSelector(getTheme);
+
+  const handleButton = () => {
+    dispatch(changeTheme());
+  };
+
+  return (
+    <StyledSwiper $themeType={theme} onClick={handleButton}>
+      <StyledSwiperBtn $themeType={theme} />
+    </StyledSwiper>
+  );
+};
+
+export { Swiper };
--- src/data/slices/sliceMemory.ts
+++ src/data/slices/sliceMemory.ts
@@ -1,8 +1,10 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 
+import { ThemeMode } from 'constants/themeMode';
 const initialState = {
   UID: '',
+  theme: ThemeMode.WHITE,
 };
 
 const sliceMemory = createSlice({
@@ -12,13 +14,17 @@ const sliceMemory = createSlice({
     changeUID: (state, action: PayloadAction<string>) => {
       state.UID = action.payload;
     },
+    changeTheme: (state) => {
+      state.theme = state.theme === ThemeMode.DARK ? ThemeMode.WHITE : ThemeMode.DARK;
+    },
   },
   selectors: {
     getUID: (state) => state.UID,
+    getTheme: (state) => state.theme,
   },
 });
 
-export const { changeUID } = sliceMemory.actions;
-export const { getUID } = sliceMemory.selectors;
+export const { changeUID, changeTheme } = sliceMemory.actions;
+export const { getUID, getTheme } = sliceMemory.selectors;
 
 export default sliceMemory.reducer;
