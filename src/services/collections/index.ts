ddff3b15fc9f9bb5d59a2115695a62d920cfc5a7
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-01 16:51:08 +0300

    feat: add logic for change user`s profile

--- src/services/collections/index.ts
+++ src/services/collections/index.ts
@@ -1,8 +1,11 @@
 import { type User } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
-import { db } from 'services/firebase';
+import { serviceAuthentication } from 'services/auth';
+import { db, storage } from 'services/firebase';
 import { stringDate } from 'utils/getDateValues';
+import { TypesFormModal, TypeUser } from 'types';
 
 class CollectionsService {
   async addUserWithEmail(
@@ -16,8 +19,22 @@ class CollectionsService {
   ) {
     try {
       setDoc(doc(db, 'users', uid), {
-        data: { ...data, uid },
+        ...data,
         date: stringDate(date),
+        uid,
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async addUserWithGoogle(uid: string, user: User) {
+    try {
+      setDoc(doc(db, 'users', uid), {
+        name: user.displayName,
+        number: user?.phoneNumber,
+        uid: user?.uid,
+        photo: user?.photoURL,
       });
     } catch (error) {
       console.log(error);
@@ -25,15 +42,48 @@ class CollectionsService {
     }
   }
 
-  async addUserWithGoogle(uid: string, user: User) {
+  async viewUser(uid: string) {
     try {
-      setDoc(doc(db, 'users', uid), {
-        data: {
-          name: user.displayName,
-          number: user?.phoneNumber,
-          img: user?.photoURL,
-          uid: user?.uid,
-        },
+      const docRef = doc(db, 'users', uid);
+      const docSnap = await getDoc(docRef);
+      return docSnap.data() as TypeUser;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async updateAvatar(
+    user: TypeUser,
+    data: TypesFormModal,
+    gender: string,
+    uid: string,
+    file: File
+  ) {
+    try {
+      if (data.password) serviceAuthentication.changePassword(data.password);
+      const mountainsRef = ref(storage, file.name);
+      await uploadBytes(mountainsRef, file);
+      const downloadURL = await getDownloadURL(mountainsRef);
+      await updateDoc(doc(db, 'users', uid), {
+        ...user,
+        ...data,
+        gender,
+        photo: downloadURL,
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async updateProfile(user: TypeUser, data: TypesFormModal, gender: string, uid: string) {
+    try {
+      if (data.password) serviceAuthentication.changePassword(data.password);
+      await updateDoc(doc(db, 'users', uid), {
+        ...user,
+        ...data,
+        gender,
       });
     } catch (error) {
       console.log(error);
