a0164bca909782b3fc14829c758ebd56aa9a256b
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-01 16:58:42 +0300

    feat: add user info

---
+++ src/components/User/UserInfo/index.tsx
@@ -0,0 +1,52 @@
+import back from 'assets/images/back-profile.webp';
+import plug from 'assets/images/plug.svg';
+import { type PropsUserInfo } from 'types';
+
+import { EditButton } from '../EditButton';
+import { UserModal } from '../UserModal';
+
+import {
+  StyledUserInfo,
+  StyledUserInfoAvatar,
+  StyledUserInfoAvatarWraper,
+  StyledUserInfoBack,
+  StyledUserInfoBackWrapper,
+  StyledUserInfoBlock,
+  StyledUserInfoBlockName,
+  StyledUserInfoBlockP,
+  StyledUserInfoBlockSpan,
+  StyledUserInfoBlockTg,
+} from './styled';
+
+const UserInfo: React.FC<PropsUserInfo> = ({ user, viewModal, changeViewModal }) => {
+  const { photo, name, date, secondName, linkTG, gender } = user;
+
+  return (
+    <StyledUserInfo>
+      <StyledUserInfoBackWrapper>
+        <StyledUserInfoBack src={back} alt="flowers" />
+      </StyledUserInfoBackWrapper>
+      <StyledUserInfoBlock>
+        <StyledUserInfoAvatarWraper>
+          <StyledUserInfoAvatar src={photo ? photo : plug} alt="avatar" />
+        </StyledUserInfoAvatarWraper>
+        <StyledUserInfoBlockName>
+          {name} {secondName}
+        </StyledUserInfoBlockName>
+        <StyledUserInfoBlockP>
+          Date: <StyledUserInfoBlockSpan>{date}</StyledUserInfoBlockSpan>
+        </StyledUserInfoBlockP>
+        <StyledUserInfoBlockP>
+          Gender: <StyledUserInfoBlockSpan>{gender}</StyledUserInfoBlockSpan>
+        </StyledUserInfoBlockP>
+        <StyledUserInfoBlockP>
+          TG: <StyledUserInfoBlockTg>{linkTG}</StyledUserInfoBlockTg>
+        </StyledUserInfoBlockP>
+      </StyledUserInfoBlock>
+      <EditButton changeModal={changeViewModal} />
+      {viewModal && <UserModal user={user} changeModal={changeViewModal} />}
+    </StyledUserInfo>
+  );
+};
+
+export { UserInfo };
