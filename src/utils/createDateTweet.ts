fbe4754218521bf0f62329ab27481071c6daa0f9
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-02 16:05:40 +0300

    feat: add get time for tweet

---
+++ src/utils/createDateTweet.ts
@@ -0,0 +1,13 @@
+const getTime = (timestamp: number) => {
+  const date = new Date(timestamp);
+
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  const month = date.toLocaleString('en-US', { month: 'short' }); // Формат для английского языка
+  const day = date.getDate();
+
+  return `${hours}:${minutes} ${month} ${day}`;
+};
+
+export { getTime };
