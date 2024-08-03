4672a045020c9ec5913b157ed5aa00c20600b984
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-03 20:51:10 +0300

    feat: add sidebar with create tweet

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+import { ThemeMode } from 'constants/themeMode';
+import { useAppSelector } from 'hooks/useRedux';
+import crossGray from 'assets/icons/cross-gray.svg';
+import crossWhite from 'assets/icons/cross-white.svg';
+import { getTheme } from 'data/slices/sliceMemory';
+import { type PropsCreateTweet } from 'types';
+
+import { CreateTweet } from 'components/Tweets/CreateTweet';
+
+import {
+  StlyedSidebar,
+  StlyedSidebarBtnTweet,
+  StlyedSidebarModal,
+  StlyedSidebarModalClouse,
+  StlyedSidebarModalH2,
+} from './styled';
+
+const Sidebar: React.FC<PropsCreateTweet> = ({ user, uid, addNewTweet }) => {
+  const [modal, setModal] = useState(false);
+
+  const theme = useAppSelector(getTheme);
+
+  const handleModal = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <StlyedSidebar $modal={modal} $typeTheme={theme}>
+      <StlyedSidebarBtnTweet onClick={handleModal}>Tweet</StlyedSidebarBtnTweet>
+      {modal && (
+        <StlyedSidebarModal>
+          <StlyedSidebarModalH2>Create your tweet</StlyedSidebarModalH2>
+          <CreateTweet user={user} uid={uid} addNewTweet={addNewTweet} />
+          <StlyedSidebarModalClouse
+            onClick={handleModal}
+            src={theme === ThemeMode.WHITE ? crossGray : crossWhite}
+            alt="cross"
+          />
+        </StlyedSidebarModal>
+      )}
+    </StlyedSidebar>
+  );
+};
+
+export { Sidebar };
