134ce512c60cd9516a9f9fcd754d239e2317447e
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-05 11:00:02 +0300

    feat: add constants for menu

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,5 +5,16 @@ import { PATTERNS } from './formPatterns';
 import { GENDERS } from './genders';
 import { Paths } from './paths';
 import { ThemeMode } from './themeMode';
+import { menuItems } from './menuItems';
 
-export { Paths, ERRORS, PATTERNS, DATE_PERIODS, NAMES_PERIODS, BREAKPOINTS, ThemeMode, GENDERS };
+export {
+  Paths,
+  ERRORS,
+  PATTERNS,
+  DATE_PERIODS,
+  NAMES_PERIODS,
+  BREAKPOINTS,
+  ThemeMode,
+  GENDERS,
+  menuItems,
+};
---
+++ src/constants/menuItems.ts
@@ -0,0 +1,21 @@
+import home from 'assets/icons/sidebar/home.svg';
+import explore from 'assets/icons/sidebar/explore.svg';
+import notifications from 'assets/icons/sidebar/notifications.svg';
+import messages from 'assets/icons/sidebar/messages.svg';
+import bookmarks from 'assets/icons/sidebar/bookmarks.svg';
+import lists from 'assets/icons/sidebar/lists.svg';
+import profile from 'assets/icons/sidebar/profile.svg';
+import more from 'assets/icons/sidebar/more.svg';
+
+const menuItems = [
+  { img: home, name: 'Home', path: '/feed' },
+  { img: explore, name: 'Explore' },
+  { img: notifications, name: 'Notifications' },
+  { img: messages, name: 'Messages' },
+  { img: bookmarks, name: 'Bookmarks' },
+  { img: lists, name: 'Lists' },
+  { img: profile, name: 'Profile', path: '/profile' },
+  { img: more, name: 'More' },
+];
+
+export { menuItems };
