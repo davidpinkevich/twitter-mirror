9d87cfda5f25e6dc3f09590bb156ad2e0eb4238d
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-07 17:48:02 +0300

    feat: add styles feed header

---
+++ src/components/User/FeedHeader/styled.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+const StyledUserHeaderWrap = styled.header`
+  max-width: 750px;
+  position: relative;
+  padding-bottom: 20px;
+`;
+
+const StyledUserHeaderWrapImg = styled.img`
+  display: inline-block;
+  max-width: 750px;
+  object-fit: contain;
+`;
+
+const StyledUserHeaderWrapAvatar = styled.img`
+  position: relative;
+  bottom: 40px;
+  left: 40px;
+  z-index: 5;
+  border-radius: 50%;
+  width: 125px;
+  height: 125px;
+  object-fit: cover;
+`;
+
+const StyledUserHeader = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+const StyledUserHeaderBack = styled.button`
+  background-color: inherit;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: inherit;
+
+  &:hover {
+    & img {
+      transform: scale(1.1);
+    }
+
+    & p {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const StyledUserHeaderBackImg = styled.img`
+  display: inline-block;
+  transition: all 0.2s;
+`;
+
+const StyledUserHeaderBackText = styled.p`
+  transition: all 0.2s;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export {
+  StyledUserHeader,
+  StyledUserHeaderBack,
+  StyledUserHeaderWrap,
+  StyledUserHeaderBackImg,
+  StyledUserHeaderBackText,
+  StyledUserHeaderWrapImg,
+  StyledUserHeaderWrapAvatar,
+};
