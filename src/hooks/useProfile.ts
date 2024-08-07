b4f6384284341cae916b110ac253066f40a5a7b4
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-07 13:23:43 +0300

    feat: update hook for profile page

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 
 import { getUID } from 'data/slices/sliceMemory';
-import { type TypeTweet,type TypeUser } from 'types';
+import { type TypeTweet, type TypeUser } from 'types';
 
 import { useAppSelector } from './useRedux';
 
 const useProfile = () => {
   const uid = useAppSelector(getUID);
   const [viewModal, setViewModal] = useState(false);
+  const [targetTweet, setTargetTweet] = useState<null | TypeTweet>(null);
   const [user, setUser] = useState<TypeUser>({ gender: 'secret', tweets: [], uid: '' });
 
   const [tweets, setTweets] = useState<TypeTweet[]>([]);
 
-  const [observerLikes, setObserverLikes] = useState(0);
+  const [observerLikes, setObserverLikes] = useState(false);
 
   return {
     uid,
@@ -24,6 +25,8 @@ const useProfile = () => {
     setTweets,
     observerLikes,
     setObserverLikes,
+    targetTweet,
+    setTargetTweet,
   };
 };
 
