144617856d65fda62c8598d8faff7f30e34a2713
davidpinkevich <davidpinkevich95@gmail.com>
2024-08-07 17:48:51 +0300

    feat: add styles feed page

---
+++ src/pages/Feed/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledFeed = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 20px;
+`;
+
+const StyledFeedMain = styled.div`
+  flex-grow: 1;
+  min-width: 750px;
+`;
+
+export { StyledFeed, StyledFeedMain };
